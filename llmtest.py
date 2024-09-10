ea032b9100868c2a1cba2308ed332b60ed99c148
dorus <doruskeijzer@gmail.com>
2024-09-10 12:10:15 +0200

    made a start to LLM call

---
+++ llmtest.py
@@ -0,0 +1,22 @@
+from llama_cpp import Llama
+
+llm = Llama.from_pretrained(
+      # seed=1337, # Uncomment to set a specific seed
+      # n_ctx=2048, # Uncomment to increase the context window
+      repo_id="Qwen/Qwen2-0.5B-Instruct-GGUF",
+      n_gpu_layers=-1, # Uncomment to use GPU acceleration
+      filename="*q8_0.gguf",
+      verbose=False
+)
+output = llm.create_chat_completion(
+      messages = [
+          {"role": "system", "content": "You are an assistant who perfectly describes images."},
+          {
+              "role": "user",
+              "content": "Write a limerick about machine learning"
+          }
+      ]
+)
+
+
+print(output)
